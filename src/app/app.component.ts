18e67cfb72b3430a0f11ebd49803dfaf2c06eaee
Jongsik <jongsik@cloudflare.com>
2025-02-24 15:40:15 +0900

    added: dynamic turnstile call example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'turnstile';
-  sendCaptchaResponse(captchaResponse: string | null) {
-    if (captchaResponse === null) {
-      return;
+
+  ngOnInit() {
+    (window as any).turnstileCB = this.turnstileCB.bind(this);
+    this.loadTurnstileScript();
+  }
+
+  loadTurnstileScript() {
+    console.log('Loading Turnstile script');
+    const script = document.createElement('script');
+    script.src =
+      'https://challenges.cloudflare.com/turnstile/v0/api.js?onload=turnstileCB';
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+  }
+
+  turnstileCB() {
+    console.log('turnstileCB called');
+    const turnstile = (window as any).turnstile;
+    console.log('turnstile', turnstile);
+    if (turnstile) {
+      console.log('turnstile', turnstile);
+
+      turnstile.render('#turnstileSample', {
+        sitekey: '0x4AAAAAAA9I3SJpjradfKzn',
+        theme: 'dark',
+      });
+    } else {
+      console.error('Turnstile is not available on the window object.');
     }
-    console.log(`Resolved captcha with response: ${captchaResponse}`);
+  }
+
+  handleTurnstileClick() {
+    const token = (window as any).turnstile.getResponse();
+    const textArea = document.getElementById('textArea');
+    if (textArea) {
+      textArea.innerText =
+        '\n### TOKEN BEGIN ###\n' +
+        textArea.innerText +
+        token +
+        '\n### TOKEN END\n';
+    }
   }
 }
